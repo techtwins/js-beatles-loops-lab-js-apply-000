bb1215899c5b15fdedc97d80c4e837fb176457e8
Shevaughn Grant <gshevaughn@gmail.com>
2020-08-18 20:41:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var text = [];
+  var a = [];
 
   for (var i = 0; i < musicians.length; i++) {
-    text[i] = `${musicians[i]} plays ${instruments[i]}`;
+    a[i] = `${musicians[i]} plays ${instruments[i]}`;
   }
 
-  return text;
+  return a;
 }
 
 function johnLennonFacts(facts){
-  var text = [];
+  var a = [];
 
   var i = 0;
   while (i < facts.length) {
-    text[i] = facts[i] + "!!!";
+    a[i] = facts[i] + "!!!";
     i++;
   }
 
-  return text;
+  return a;
 }
 
 function iLoveTheBeatles(count){
-  var text = [];
+  var a = [];
 
   if(count == 17){
-    text[0] = 'I love the Beatles!'
-    return text;
+    a[0] = 'I love the Beatles!'
+    return a;
   }
 
   var i = 0
